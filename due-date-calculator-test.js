2d68f03cae7c0d377369e64cb71496a94b13edcc
Balint Csizmadia <balintcsizmadia1@gmail.com>
2020-05-12 19:56:16 +0200

    Minor addition

--- due-date-calculator-test.js
+++ due-date-calculator-test.js
@@ -3,7 +3,7 @@ describe("Testing the functionality", () => {
   beforeEach(() => {
     calculator = new DueDateCalculator();
   }),
-    it("should calculate due date: May 8, 2020, 14:00 -> May 12, 2020, 14:00", () => {
+    it("should calculate due date: May 8, 2020, 14:00 -> Turnaround time: 16h -> May 12, 2020, 14:00", () => {
       const submitDate = new Date("May 8, 2020");
       submitDate.setHours(14);
       const result = calculator.calculateDueDate(submitDate, 16);
@@ -12,7 +12,7 @@ describe("Testing the functionality", () => {
       wantedResult.setHours(14);
       expect(result.getTime()).toBe(wantedResult.getTime());
     }),
-    it("should calculate due date: May 5, 2020, 10:00 -> May 8, 2020, 16:00", () => {
+    it("should calculate due date: May 5, 2020, 10:00 -> Turnaround time: 30h -> May 8, 2020, 16:00", () => {
       const submitDate = new Date("May 5, 2020");
       submitDate.setHours(10);
       const result = calculator.calculateDueDate(submitDate, 30);
@@ -21,7 +21,7 @@ describe("Testing the functionality", () => {
       wantedResult.setHours(16);
       expect(result.getDate()).toBe(wantedResult.getDate());
     }),
-    it("should calculate due date: Aug 28, 2020, 15:00 -> Sep 2, 2020, 15:00", () => {
+    it("should calculate due date: Aug 28, 2020, 15:00 -> Turnaround time: 24h -> Sep 2, 2020, 15:00", () => {
       const submitDate = new Date("Aug 28, 2020");
       submitDate.setHours(15);
       const result = calculator.calculateDueDate(submitDate, 24);
